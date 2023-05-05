ee698eef69a05a07b01389a6e1dea8051357ef6e
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-05-05 22:29:44 +0300

    contact form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,29 @@
 import css from './App.module.css'
+import ContactsForm from '../ContactsForm/ContactsForm';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <>
-      <header className={css.block}>
-        <p className={css.textMain}>REACT APP TEMPLATE</p>
-        <p className={css.textSecondary}>USE IT</p>
-      </header>
-    </>
-  );
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: ''
+  }
+
+  onHandleAddContact = (contact) => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact]
+    }))
+
+    console.log(this.state.contacts)
+  }
+
+  render() {
+    return (
+      <div className={css.container}>
+        <ContactsForm onSubmit={this.onHandleAddContact} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import css from './ContactsForm.module.css';
+
+class ContactsForm extends Component {
+    state = {
+        name: '',
+        number: '',
+    }
+
+    onInputChange = e => {
+        this.setState({[e.currentTarget.name]: e.currentTarget.value})
+    }
+
+    onFormSubmit = e => {
+        e.preventDefault()
+
+        this.props.onSubmit(this.state)
+        
+        this.setState({name: '', number: ''})
+    }
+
+    render() {
+        return (
+            <>
+                <h1 className={css.title}>Add new contact</h1>
+                <form className={css.form} autoComplete="off" onSubmit={e => this.onFormSubmit(e)}>
+                    <div className={css.inputField}>
+                        <input
+                            className={css.input} 
+                            placeholder="."
+                            type="text" 
+                            name="name" 
+                            id="name" 
+                            value={this.state.name} 
+                            onChange={e => this.onInputChange(e)}
+                            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                            required
+                        />
+                        <label htmlFor="name" className={css.label}>Name</label>
+                    </div>
+                    <div className={css.inputField}>
+                        <input
+                            className={css.input} 
+                            placeholder="."
+                            type="tel" 
+                            name="number" 
+                            id="number" 
+                            value={this.state.number} 
+                            onChange={e => this.onInputChange(e)}
+                            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                            required 
+                        />
+                        <label htmlFor="number" className={css.label}>Number</label>
+                    </div>
+                    <button className={css.button} type="submit">Add contact</button>
+                </form>
+            </>
+        )
+    }
+}
+
+export default ContactsForm
